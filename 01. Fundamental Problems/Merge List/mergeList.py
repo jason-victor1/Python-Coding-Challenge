1079083175ca3b9777db4dcdc6973b639fe23bd5
Jason Victor <jason_victor@outlook.com>
2025-02-09 23:33:38 -0500

    added source and pseudo code files

---
+++ 01. Fundamental Problems/Merge List/mergeList.py
@@ -0,0 +1,10 @@
+def mergeList(first, second):
+    combined = first + second
+    combined.sort()
+    return combined
+
+
+group1 = [11, 13, 18, 17, 56]
+group2 = [12, 15, 19, 43, 66]
+merged = mergeList(group1, group2)
+print(merged)
