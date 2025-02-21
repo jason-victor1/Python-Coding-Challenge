60d1e0baaa861aeeed299fca69f8177148025c9b
Jason Victor <jason_victor@outlook.com>
2025-02-20 20:53:45 -0500

    added source code file

---
+++ 02. Easy Ones/Random Number/random_number.py
@@ -0,0 +1,7 @@
+import random  # Import the random module to access functions for generating random numbers
+
+# Generate a random integer between 0 and 10 (inclusive)
+random_num = random.randint(0, 10)
+
+# Print the generated random number to the console
+print(random_num)
