267a060532703171e63aa0ea98f730a4d851c242
Jason Victor <jason_victor@outlook.com>
2025-02-27 23:31:17 -0500

    added pseudo code file

---
+++ 02. Easy Ones/Swap Two Variables/pseudo_code.md
@@ -0,0 +1,14 @@
+START
+
+1. SET variable a to 6  
+2. SET variable b to 7
+
+3. SWAP the values of a and b  
+   - ASSIGN the value of b to a temporary variable  
+   - ASSIGN the value of a to b  
+   - ASSIGN the temporary variable to a
+
+4. DISPLAY "a =" followed by the value of a  
+5. DISPLAY "b =" followed by the value of b
+
+END
--- 02. Easy Ones/Swap Two Variables/swap_two.py
+++ 02. Easy Ones/Swap Two Variables/swap_two.py
@@ -1,9 +1,11 @@
-a = 6
-b = 7
+# Initialize variables with specific values
+a = 6  # Assign 6 to variable a
+b = 7  # Assign 7 to variable b
 
-
-# Swapping variables
+# Swapping variables using tuple unpacking
+# Here, the values of a and b are swapped in one line.
 a, b = b, a
 
-print("a =", a)
-print("b =", b)
+# Print the swapped values to the console
+print("a =", a)  # After swapping, a now holds the value that was originally in b (7)
+print("b =", b)  # After swapping, b now holds the value that was originally in a (6)
